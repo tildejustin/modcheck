ecdebea5137c0b9b9e15082f8e87164f371732b2
tildejustin <spacepepper3.14@gmail.com>
2025-09-20 18:17:55 -0400

    remove /bin

--- bin/main/com/pistacium/modcheck/util/Config.kt
+++
@@ -1,9 +0,0 @@
-package com.pistacium.modcheck.util
-
-import kotlinx.serialization.Serializable
-import java.nio.file.*
-
-@Serializable
-data class Config(val filepath: String) {
-    fun getDirectory(): Path = Paths.get(filepath)
-}
